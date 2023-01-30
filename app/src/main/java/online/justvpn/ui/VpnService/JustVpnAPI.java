b3851231cffe4ce52a8418e8719bbccb499a0107
Sergei Moskvichev <moskvicheff@gmail.com>
2023-01-30 16:19:50 +0100

    Added VPN routines NOTE: VPN now actually works but there's a lot of things to be done. I've tried to capture them in TODO

--- app/src/main/java/online/justvpn/ui/VpnService/JustVpnAPI.java
+++ app/src/main/java/online/justvpn/ui/VpnService/JustVpnAPI.java
@@ -17,6 +17,78 @@ import java.util.List;
 
 public class JustVpnAPI {
 
+    // TODO: make this class a singletone as we won't need more than 1 instances of it for HTTP
+    static final int CONTROL_ACTION_CONNECT = 0;
+    static final int CONTROL_ACTION_KEEPALIVE = 1;
+    static final int CONTROL_ACTION_DISCONNECT = 2;
+    static final int CONTROL_ACTION_GET_PARAMETERS = 3;
+    static final int CONTROL_ACTION_CONFIGURED = 4;
+    static final int CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN = 5;
+
+    public static final String CONTROL_ACTION_CONNECT_TEXT = "action:connect";
+    public static final String CONTROL_ACTION_KEEPALIVE_TEXT = "action:keepalive";
+    public static final String CONTROL_ACTION_DISCONNECT_TEXT = "action:disconnect";
+    public static final String CONTROL_ACTION_GET_PARAMETERS_TEXT = "action:getparameters";
+    public static final String CONTROL_ACTION_CONFIGURED_TEXT = "action:configured";
+    public static final String CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN_TEXT = "action:verifysubscription";
+
+    public static String actionToText(int action)
+    {
+        String action_text = "";
+        switch (action)
+        {
+            case CONTROL_ACTION_CONNECT:
+                action_text = CONTROL_ACTION_CONNECT_TEXT;
+                break;
+            case CONTROL_ACTION_KEEPALIVE:
+                action_text = CONTROL_ACTION_KEEPALIVE_TEXT;
+                break;
+            case CONTROL_ACTION_DISCONNECT:
+                action_text = CONTROL_ACTION_DISCONNECT_TEXT;
+                break;
+            case CONTROL_ACTION_GET_PARAMETERS:
+                action_text = CONTROL_ACTION_GET_PARAMETERS_TEXT;
+                break;
+            case CONTROL_ACTION_CONFIGURED:
+                action_text = CONTROL_ACTION_CONFIGURED_TEXT;
+                break;
+            case CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN:
+                action_text = CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN_TEXT;
+                break;
+            default:
+                break;
+        }
+        return action_text;
+    }
+
+    public static int actionFromText(String action)
+    {
+        int act = -1;
+        switch (action) {
+            case CONTROL_ACTION_CONNECT_TEXT:
+                act = CONTROL_ACTION_CONNECT;
+                break;
+            case CONTROL_ACTION_KEEPALIVE_TEXT:
+                act = CONTROL_ACTION_KEEPALIVE;
+                break;
+            case CONTROL_ACTION_DISCONNECT_TEXT:
+                act = CONTROL_ACTION_DISCONNECT;
+                break;
+            case CONTROL_ACTION_GET_PARAMETERS_TEXT:
+                act = CONTROL_ACTION_GET_PARAMETERS;
+                break;
+            case CONTROL_ACTION_CONFIGURED_TEXT:
+                act = CONTROL_ACTION_CONFIGURED;
+                break;
+            case CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN_TEXT:
+                act = CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN;
+                break;
+            default:
+                break;
+        }
+        return act;
+    }
+
     public interface onServersDownloadedInterface {
         void onServersDownloadReady(List<JustVpnAPI.ServerDataModel> servers);
     }
@@ -55,7 +127,7 @@ public class JustVpnAPI {
                             }
                             servers.add(server);
                         }
-                        /*TimeUnit.SECONDS.sleep(5);*/
+
                         callback.onServersDownloadReady(servers);
                     }
                     catch (JSONException e)
@@ -65,9 +137,8 @@ public class JustVpnAPI {
                 }
                 , error ->
                 {
-                    // restart
+                    // TODO: Retry? I don't know what to do in this case.
                     Log.w("JustVpn API error: ", error);
-
                 });
 
         queue.add(signalRequest);
--- app/src/main/java/online/justvpn/ui/VpnService/JustVpnConnection.java
+++ app/src/main/java/online/justvpn/ui/VpnService/JustVpnConnection.java
@@ -5,6 +5,8 @@ import static java.nio.charset.StandardCharsets.US_ASCII;
 import android.net.VpnService;
 import android.os.ParcelFileDescriptor;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -12,16 +14,25 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 public class JustVpnConnection implements Runnable {
-    enum ControlAction {
-        CONTROL_ACTION_CONNECT,
-        CONTROL_ACTION_KEEPALIVE,
-        CONTROL_ACTION_DISCONNECT,
-        CONTROL_ACTION_GET_PARAMETERS,
-        CONTROL_ACTION_CONFIGURED,
-        CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN
+    private Thread mReceiverThread;
+    ParcelFileDescriptor mVPNInterface = null;
+    private int mMTU;
+
+    enum CONNECTION_STATE
+    {
+        CONNECTION_STATE_IDLE,          // default
+        CONNECTION_STATE_CONNECTING,    // this connection is being initialized
+        CONNECTION_STATE_DISCONNECTING, // the client is disconnecting
+        CONNECTION_STATE_CONNECTED,     // handshake complete and the interface is configured
+        CONNECTION_STATE_ACTIVE,        // connection is established completely and IP the packets are being forwarded
+        CONNECTION_STATE_DISCONNECTED,  // the client is not connected anymore
+        CONNECTION_STATE_NO_SLOTS,      // selected server cannot handle any more connections as it's full
+        CONNECTION_STATE_FAILED         // connection attempt has failed by some reason
     }
+    private CONNECTION_STATE mConnectionState = CONNECTION_STATE.CONNECTION_STATE_IDLE;
     private VpnService.Builder mBuilder;
     private VpnService mService;
     private DatagramChannel mServerChannel;
@@ -42,24 +53,25 @@ public class JustVpnConnection implements Runnable {
             final SocketAddress serverAddress = new InetSocketAddress(InetAddress.getByName("justvpn.online"), 8811);
             start(serverAddress);
         }
-        catch (IOException | IllegalArgumentException  | IllegalStateException e)
+        catch (IOException | IllegalArgumentException | IllegalStateException | InterruptedException e)
         {
-            // TODO: notify
+            // TODO: notify the service
         }
         finally
         {
-            // TODO: notify
+            // TODO: notify the service
         }
     }
 
-    private void start(SocketAddress server) throws IOException {
-
+    private void start(SocketAddress server) throws IOException, InterruptedException {
+        setConnectionState(CONNECTION_STATE.CONNECTION_STATE_CONNECTING);
         // Connect to the server
         mServerChannel = DatagramChannel.open();
         DatagramSocket socket = mServerChannel.socket();
 
         if (!mService.protect(socket)) // fails if user permissions are not given, see requestVpnServicePermissionDialog()
         {
+            setConnectionState(CONNECTION_STATE.CONNECTION_STATE_FAILED);
             throw new IllegalStateException("Cannot protect the tunnel");
         }
 
@@ -80,17 +92,106 @@ public class JustVpnConnection implements Runnable {
             return;
         }
 
+        setConnectionState(CONNECTION_STATE.CONNECTION_STATE_CONNECTED);
+
         // start VPN routine
-        ParcelFileDescriptor vpnInterface = mBuilder.establish();
+        mVPNInterface = mBuilder.establish();
 
         // Tell the server we're configured
-        sendControlMsg(ControlAction.CONTROL_ACTION_CONFIGURED);
+        sendControlMsg(JustVpnAPI.CONTROL_ACTION_CONFIGURED);
 
         // Verify subscription on server side
         // token=debug_debug will allow the server to accept this connection even if this is an unpaid one
-        sendControlMsg(ControlAction.CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN, "token=debug_debug");
+        sendControlMsg(JustVpnAPI.CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN, "token=debug_debug");
 
-        // Start VPN routine TBD
+        setConnectionState(CONNECTION_STATE.CONNECTION_STATE_ACTIVE);
+
+        // Start VPN routine
+        startReceiverThread();
+
+        // ... and process outgoing packages
+        int nEmptyPacketCounter = 0;
+        FileInputStream in = new FileInputStream(mVPNInterface.getFileDescriptor());
+        ByteBuffer packet = ByteBuffer.allocate(mMTU);
+
+        while (mConnectionState == CONNECTION_STATE.CONNECTION_STATE_ACTIVE)
+        {
+            // Read the outgoing packet from the input stream.
+            int length = in.read(packet.array());
+            if (length > 0)
+            {
+                // Write the outgoing packet to the tunnel.
+                packet.limit(length);
+                mServerChannel.write(packet);
+                packet.clear();
+                nEmptyPacketCounter = 0;
+            }
+            else
+            {
+                nEmptyPacketCounter++;
+                if (nEmptyPacketCounter > 15)
+                {
+                    // To minimize busy looping, sleep thread if data remains 0 for a few cycles
+                    Thread.sleep(TimeUnit.MILLISECONDS.toMillis(100));
+                }
+            }
+        }
+    }
+
+    private void startReceiverThread()
+    {
+        mReceiverThread = new Thread(() ->
+        {
+            try
+            {
+                ByteBuffer packet = ByteBuffer.allocate(mMTU);
+                FileOutputStream out = new FileOutputStream(mVPNInterface.getFileDescriptor());
+
+                while (mConnectionState == CONNECTION_STATE.CONNECTION_STATE_ACTIVE)
+                {
+                    // Read the incoming packet from the tunnel.
+                    int len = mServerChannel.read(packet);
+                    if (len > 0)
+                    {
+                        // Ignore control messages, which start with zero.
+                        if (packet.get(0) == 0)
+                        {
+                            ProcessControl(packet, len);
+                        }
+                        // whatever comes from the server, put into the system's out stream
+                        else
+                        {
+                            out.write(packet.array(), 0, len);
+                        }
+                        packet.clear();
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        mReceiverThread.start();
+    }
+
+    private void ProcessControl(ByteBuffer p, int len) throws IOException {
+        switch (JustVpnAPI.actionFromText(new String(p.array(), 1, len -1)))
+        {
+            case JustVpnAPI.CONTROL_ACTION_KEEPALIVE:
+                // Response to the server with keepalive
+                sendControlMsg(JustVpnAPI.CONTROL_ACTION_KEEPALIVE);
+                break;
+            // TODO: Make server to send "action:disconnect" to the client that has not responded to keepalives.
+            default:
+                break;
+        }
+    }
+
+    private void setConnectionState(CONNECTION_STATE state)
+    {
+        mConnectionState = state;
+        // TODO: Notify the service
     }
 
     private boolean configure() throws IOException {
@@ -101,7 +202,7 @@ public class JustVpnConnection implements Runnable {
         int nParemLen = 0;
 
         // Request network parameters from the server
-        if (sendControlMsg(ControlAction.CONTROL_ACTION_GET_PARAMETERS) > 0 && // we have sent > 0 bytes
+        if (sendControlMsg(JustVpnAPI.CONTROL_ACTION_GET_PARAMETERS) > 0 && // we have sent > 0 bytes
             (nParemLen = mServerChannel.read(packet)) > 0 && // we have read > 0 bytes
             packet.get(0) == 0)// All control messages begin with 0
 
@@ -125,6 +226,7 @@ public class JustVpnConnection implements Runnable {
                 switch (key)
                 {
                     case "mtu":
+                        mMTU = Short.parseShort(value);
                         mBuilder.setMtu(Short.parseShort(value));
                         break;
                     case "address":
@@ -139,7 +241,6 @@ public class JustVpnConnection implements Runnable {
                 }
             }
 
-
             bConfigureOK = true;
         }
 
@@ -153,7 +254,7 @@ public class JustVpnConnection implements Runnable {
         boolean bHandshakeOK = false;
         for (int i = 0; i < MAX_HANDSHAKE_ATTEMPTS; i++)
         {
-            if (sendControlMsg(ControlAction.CONTROL_ACTION_CONNECT) > 0 && // we have sent > 0 bytes
+            if (sendControlMsg(JustVpnAPI.CONTROL_ACTION_CONNECT) > 0 && // we have sent > 0 bytes
                 mServerChannel.read(packet) > 0 && // we have read > 0 bytes
                 packet.get(0) == 0 && // All control messages begin with 0
                 new String(packet.array()).contains("action:connected")) // server replied with "connected"
@@ -172,31 +273,11 @@ public class JustVpnConnection implements Runnable {
         return bHandshakeOK;
     }
 
-    int sendControlMsg(ControlAction control_action, String sParam) throws IOException {
-        ByteBuffer packet = ByteBuffer.allocate(1500);
-        String action = "";
+    int sendControlMsg(int control_action, String sParam) throws IOException {
+        ByteBuffer packet = ByteBuffer.allocate(128);
+        String action = JustVpnAPI.actionToText(control_action);
+
         int nSent = 0;
-        switch (control_action)
-        {
-            case CONTROL_ACTION_CONNECT:
-                action = "action:connect"; // upon receive, server will send back network configuration
-                break;
-            case CONTROL_ACTION_GET_PARAMETERS:
-                action = "action:getparameters";
-                break;
-            case CONTROL_ACTION_DISCONNECT:
-                break;
-            case CONTROL_ACTION_KEEPALIVE:
-                break;
-            case CONTROL_ACTION_CONFIGURED:
-                action = "action:configured";
-                break;
-            case CONTROL_ACTION_VERIFY_SUBSCRIPTION_TOKEN:
-                action = "action:verifysubscription";
-                break;
-            default:
-                break;
-        }
 
         if (!action.isEmpty())
         {
@@ -214,7 +295,7 @@ public class JustVpnConnection implements Runnable {
         return nSent;
     }
 
-    int sendControlMsg(ControlAction control_action) throws IOException {
+    int sendControlMsg(int control_action) throws IOException {
         return sendControlMsg(control_action, "");
     }
 }
